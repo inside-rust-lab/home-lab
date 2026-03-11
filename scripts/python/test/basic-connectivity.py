80fc6a30e6c45ad4c3460a954e6a862b3b88794a
eddie <eddie.eby@gmail.com>
2026-03-11 01:55:49 -0600

    add enable mode

--- scripts/python/test/basic-connectivity.py
+++ scripts/python/test/basic-connectivity.py
@@ -1,12 +1,18 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
+username = input("Username: ")
+password = getpass("Password: ")
+
 catalyst_3650 = {
     "device_type": "cisco_ios",
     "host": "192.168.20.1",
-    "username": input("Username: "),
-    "password": getpass("Password: "),
+    "username": username,
+    "password": password,
+    "secret": password
 }
 
 net_connect = ConnectHandler(**catalyst_3650)
+net_connect.enable()
+print(net_connect.find_prompt())
 net_connect.disconnect()
